b3f1d21a2176dfb0466728c62d4e962eddc4582a
jamespeterjacob <jamespeterjacob@gmail.com>
2021-03-12 05:26:53 -0800

    Add files via upload

---
+++ demo_sw.js
@@ -0,0 +1,103 @@
+//var cached_urls = [
+//    'offline.html',
+//    'index.html'
+
+//];
+//var cache_name = 'ceferin_v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('demo_v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/offline.html',
+                '/work',
+                '/contact'
+                //'/realestate.svg',
+                //'/school.svg',
+                //'/healthcare.svg',
+                //'/marketing.svg',
+                //'/md.svg',
+                //'/dm.svg',
+                //'/glgw.svg',
+                //'/photography.svg',
+                //'/entertainment.svg',
+                //'/finance.svg',
+                //'/googlelighthouse.svg',
+                //'/bg.svg',
+                //'/technology.svg',
+                //'/sports.svg',
+                //'/salon.svg',
+                //'/report.svg',
+                //'/nonprofit.svg',
+                //'/change.svg',
+                //'/consulting.svg',
+                //'/fashion.svg',
+                //'/googleIndexing.svg',
+                //'/travel.svg',
+                //'/appDevelopment.svg',
+                //'/personal.svg',
+                //'/opportunity.svg',
+                //'/OrangeLogo.svg',
+                //'/ceferinBlack.svg'
+
+            ]);
+        })
+        //.then(function (cache) {
+        //    return cache.addAll(cached_urls);
+        //})
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((resp) => {
+            return resp || fetch(event.request).then((response) => {
+                return caches.open('ceferin_v1').then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+//self.addEventListener('fetch', function (event) {
+//    console.log('Fetch event for ', event.request.url);
+//    event.respondWith(
+//        caches.match(event.request).then(function (response) {
+//            if (response) {
+//                console.log('Found ', event.request.url, ' in cache');
+//                return response;
+//            }
+//            console.log('Network request for ', event.request.url);
+//            return fetch(event.request).then(function (response) {
+//                if (response.status === 404) {
+//                    return caches.match('offline.html');
+//                }
+//                return caches.open(cached_urls).then(function (cache) {
+//                    cache.put(event.request.url, response.clone());
+//                    return response;
+//                });
+//            });
+//        }).catch(function (error) {
+//            console.log('Error, ', error);
+//            return caches.match('offline.html');
+//        })
+//    );
+//});
